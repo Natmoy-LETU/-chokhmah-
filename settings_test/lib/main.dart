cb494c780e34f3ef4993ceb17ba823169d632b46
Seth425 <sethrollins425@yahoo.com>
2024-03-25 18:19:03 -0500

    3/25/24 6:18

---
+++ settings_test/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  State<MainApp> createState() => _MainApp();
+}
+  
+
+class _MainApp extends State<MainApp>{
+String dropDownValue = 'Method 1';
+
+  @override
+  Widget build(BuildContext context){
+    return Center(
+     child: Positioned(
+      bottom: 50,
+      left: 75,
+      child: DropdownButton<String>(
+        value: dropDownValue,
+        icon: const Icon(Icons.menu),
+        style: const TextStyle(color: Colors.white),
+        underline: Container(
+          height: 2,
+          color: Colors.white,
+        ),
+        onChanged: (String? newValue){
+          setState(() {
+            dropDownValue = newValue!;
+          });
+        },
+        items: const [
+          DropdownMenuItem<String>(
+            value: 'Method 1',
+            child: Text('Method 1'),  
+          ),
+          DropdownMenuItem<String>(
+            value: 'Method 2',
+            child: Text('Method 2'),  
+          ),
+          DropdownMenuItem<String>(
+            value: 'Method 3',
+            child: Text('Method 3'),  
+          ),
+        ] 
+      ),
+     )
+    );
+  }
+}
