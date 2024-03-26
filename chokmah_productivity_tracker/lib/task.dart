e5e9c1f93e26f045c46d8fdd9e62606f77b914f2
Natmoy-LETU <101591295+Natmoy-LETU@users.noreply.github.com>
2024-03-26 00:50:26 -0500

    It works. Mostly.

--- chokmah_productivity_tracker/lib/task.dart
+++ chokmah_productivity_tracker/lib/task.dart
@@ -18,140 +18,157 @@ class _TaskState extends State<Task> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300, // Constrain the width of the task
-      child: Draggable<String>(
-        data: 'draggable_widget',
-        childWhenDragging: Container(),
-        feedback: Material(
-          elevation: 4.0,
-          child: Container(
-            color: Colors.blue.withOpacity(0.5),
-            height: taskState == TaskState.Expanded ? 200.0 : 100.0,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          taskState = taskState == TaskState.Collapsed ? TaskState.Expanded : TaskState.Collapsed;
+        });
+      },
+      child: Card(
+        elevation: 4.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // Always visible
-                  Text("<Task name>"),
-                  DropdownButton<String>(
-                    value: '% complete',
-                    onChanged: (String? newValue) {},
-                    items: [
-                      for (int i = 0; i <= 100; i += 10)
-                        DropdownMenuItem<String>(
-                          value: '$i%',
-                          child: Text('$i%'),
-                        ),
-                    ],
+                  Text("<Task Name>"),
+                  IconButton(
+                    icon: Icon(
+                      taskState == TaskState.Expanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        taskState = taskState == TaskState.Collapsed ? TaskState.Expanded : TaskState.Collapsed;
+                      });
+                    },
                   ),
-                  // Visible when expanded
-                  if (taskState == TaskState.Expanded) ...[
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(labelText: 'Dependencies'),
-                      onChanged: (String? newValue) {},
-                      items: const [
-                        DropdownMenuItem<String>(
-                          value: 'dependencies1',
-                          child: Text('<Dependencies 1>'),
-                        ),
-                        DropdownMenuItem<String>(
-                          value: 'dependencies2',
-                          child: Text('<Dependencies 2>'),
-                        ),
-                        // Add more unique values here
-                      ],
-                    ),
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(labelText: 'Resources'),
-                      onChanged: (String? newValue) {},
-                      items: const [
-                        DropdownMenuItem<String>(
-                          value: 'resources1',
-                          child: Text('<Resources 1>'),
-                        ),
-                        DropdownMenuItem<String>(
-                          value: 'resources2',
-                          child: Text('<Resources 2>'),
-                        ),
-                        // Add more unique values here
-                      ],
-                    ),
-                    DropdownButtonFormField<String>(
-                      decoration: InputDecoration(labelText: 'Status'),
-                      onChanged: (String? newValue) {},
-                      items: const [
-                        DropdownMenuItem<String>(
-                          value: 'status1',
-                          child: Text('<Status 1>'),
-                        ),
-                        DropdownMenuItem<String>(
-                          value: 'status2',
-                          child: Text('<Status 2>'),
-                        ),
-                        // Add more unique values here
-                      ],
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'Estimated Time'),
-                      keyboardType: TextInputType.number,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Action to perform when "View Subtasks" button is clicked
-                      },
-                      child: const Text('View Subtasks'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Action to perform when "Task Settings" button is clicked
-                      },
-                      child: const Text('Task Settings'),
-                    ),
-                  ],
                 ],
               ),
-            ),
-          ),
-        ),
-        child: DragTarget<String>(
-          builder: (context, candidateData, rejectedData) {
-            return Material(
-              elevation: 4.0,
-              child: InkWell(
-                onTap: () {
-                  setState(() {
-                    taskState = taskState == TaskState.Collapsed ? TaskState.Expanded : TaskState.Collapsed;
-                  });
-                },
-                child: Container(
-                  color: Colors.blue,
-                  child: Center(
-                    child: IconButton(
-                      icon: Icon(
-                        taskState == TaskState.Expanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          taskState = taskState == TaskState.Collapsed ? TaskState.Expanded : TaskState.Collapsed;
-                        });
-                      },
+              if (taskState == TaskState.Expanded) ...[
+                const SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(labelText: '% Complete'),
+                  onChanged: (String? newValue) {},
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: '% complete',
+                      child: Text('% complete'),
                     ),
-                  ),
+                    DropdownMenuItem<String>(
+                      value: '10%',
+                      child: Text('10%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '20%',
+                      child: Text('20%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '30%',
+                      child: Text('30%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '40%',
+                      child: Text('40%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '50%',
+                      child: Text('50%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '60%',
+                      child: Text('60%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '70%',
+                      child: Text('70%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '80%',
+                      child: Text('80%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '90%',
+                      child: Text('90%'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: '100%',
+                      child: Text('100%'),
+                    ),
+                  ],
                 ),
-              ),
-            );
-          },
-          onWillAccept: (data) {
-            // Implement your onWillAccept logic
-            return true;
-          },
-          onLeave: (data) {
-            // Implement your onLeave logic
-          },
-          onAccept: (data) {
-            // Implement your onAccept logic
-          },
+                const SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(labelText: 'Dependencies'),
+                  onChanged: (String? newValue) {},
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: 'dependencies1',
+                      child: Text('<Dependencies 1>'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'dependencies2',
+                      child: Text('<Dependencies 2>'),
+                    ),
+                    // Add more unique values here
+                  ],
+                ),
+                const SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(labelText: 'Resources'),
+                  onChanged: (String? newValue) {},
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: 'resources1',
+                      child: Text('<Resources 1>'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'resources2',
+                      child: Text('<Resources 2>'),
+                    ),
+                    // Add more unique values here
+                  ],
+                ),
+                const SizedBox(height: 8),
+                DropdownButtonFormField<String>(
+                  decoration: const InputDecoration(labelText: 'Status'),
+                  onChanged: (String? newValue) {},
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: 'status1',
+                      child: Text('<Status 1>'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'status2',
+                      child: Text('<Status 2>'),
+                    ),
+                    // Add more unique values here
+                  ],
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Estimated Time'),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    // Action to perform when "View Subtasks" button is clicked
+                  },
+                  child: const Text('View Subtasks'),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    // Action to perform when "Task Settings" button is clicked
+                  },
+                  child: const Text('Task Settings'),
+                ),
+              ],
+            ],
+          ),
         ),
       ),
     );
